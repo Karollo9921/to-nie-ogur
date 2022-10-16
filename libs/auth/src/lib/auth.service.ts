9b537ef7643ceddc4eb835715b5276e8039d4a2f
Karol Kluba <karol.kluba89@gmail.com>
2022-10-16 19:59:07 +0200

    feat: create auth service

--- libs/auth/src/lib/auth.service.ts
+++ libs/auth/src/lib/auth.service.ts
@@ -1,6 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '@mur-zynski/users';
+import { Response } from 'express';
+import { IJWTPayload } from './interfaces/jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
-  
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(
+    user: User,
+    response: Response
+  ): Promise<void> {
+    const jwtPayload: IJWTPayload = {
+      email: user.email,
+      login: user.login
+    }
+
+    const expiresIn = new Date();
+    expiresIn.setSeconds(
+      expiresIn.getSeconds() + this.configService.get('JWT_EXPIRATION'),
+    );
+
+    const token = this.jwtService.sign(jwtPayload);
+
+    response.cookie('Authentication', token, {
+      httpOnly: true,
+      expires: expiresIn,
+    });
+  }
 }
--- libs/users/src/index.ts
+++ libs/users/src/index.ts
@@ -1,2 +1,4 @@
 export * from './lib/users.service';
 export * from './lib/users.module';
+
+export * from './lib/entity/user.entity';
